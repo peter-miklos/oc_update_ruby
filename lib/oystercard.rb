d7a2475f0455d7a3555ec483316e410f451a498f
Peter Miklos <pt.miklos@gmail.com>
2017-02-18 21:02:45 +0000

    top_up related tests and logic added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,21 @@
 class Oystercard
 
-  def initialize
+  MAX_BALANCE = 90
+
+  def initialize(max_balance = MAX_BALANCE)
     @balance = 0
+    @max_balance = max_balance
   end
+
+  def top_up(amount)
+    fail "You cannot exceed the max balance (£#{@max_balance})" if max_balance_exceeded?(amount)
+    @balance += amount
+  end
+
+  private
+
+  def max_balance_exceeded?(amount)
+    (@balance + amount) > @max_balance
+  end
+
 end
