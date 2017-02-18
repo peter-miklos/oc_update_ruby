494c5eb4830919c5bf45a2e4a603dce8d0bc68f7
Peter Miklos <pt.miklos@gmail.com>
2017-02-18 23:58:46 +0000

    oystercard updated

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,21 +1,32 @@
 class Oystercard
 
+  MIN_BALANCE = 1
   MAX_BALANCE = 90
 
-  def initialize(max_balance = MAX_BALANCE)
+  def initialize(journey_log = JourneyLog.new)
     @balance = 0
-    @max_balance = max_balance
+    @journey_log = journey_log
   end
 
   def top_up(amount)
-    fail "You cannot exceed the max balance (£#{@max_balance})" if max_balance_exceeded?(amount)
+    fail "You cannot exceed the max balance (£#{MAX_BALANCE})" if max_balance_exceeded?(amount)
     @balance += amount
+  end
+
+  def touch_in(station)
+    fail "Your balance is too low, please top-up" if @balance <= MIN_BALANCE
+    @journey_log.start(station)
+  end
+
+  def touch_out(station)
+    fare = @journey_log.finish(station)
+    deduct(fare)
   end
 
   private
 
   def max_balance_exceeded?(amount)
-    (@balance + amount) > @max_balance
+    (@balance + amount) > MAX_BALANCE
   end
 
   def deduct(amount)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
-  let(:card) { described_class.new }
+  let(:journey_log) {double(:journey_log, start: true, finish: 1)}
+  let(:station_1) {double :station_1}
+  let(:station_2) {double :station_2}
+  let(:card) { described_class.new(journey_log) }
 
   describe "balance" do
     it "has 0 initially" do
@@ -17,16 +20,42 @@ describe Oystercard do
     end
 
     it "doesn't let user to have higher balance than the max" do
-      max_b = card.instance_variable_get("@max_balance")
+      max_b = Oystercard::MAX_BALANCE
       expect{card.top_up(91)}.to raise_error("You cannot exceed the max balance (£#{max_b})")
     end
   end
 
   describe "touch_in" do
+    it "calls the start method on journey_log with a station" do
+      card.top_up(20)
+      expect(journey_log).to receive(:start).with(station_1)
+      card.touch_in(station_1)
+    end
 
+    it "raises error if the balance is too low to enter the station" do
+      expect{card.touch_in(station_1)}.to raise_error("Your balance is too low, please top-up")
+    end
+
+    it "deducts the possible penalty charge if touch_out was missed" do
+      # TBD
+    end
   end
 
   describe "touch_out" do
 
+    before(:each) do
+      card.top_up(20)
+      card.touch_in(station_1)
+    end
+
+    it "calls the finish method on journey_log with a station" do
+      expect(journey_log).to receive(:finish).with(station_2)
+      card.touch_out(station_2)
+    end
+
+    it "deducts the fare from the balance" do
+      card.touch_out(station_2)
+      expect(card.instance_variable_get("@balance")).to eq 19
+    end
   end
 end
